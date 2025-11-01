df61e5c6696968925c2982bb48ec40c652ef46d2
grillo78 <sr.grillo79@gmail.com>
2025-11-01 19:44:14 +0100

    fixed orc eyes

--- src/main/java/grillo78/fantasy_beyond/character/customization/race/merfolk/MerfolkEyes.java
+++ src/main/java/grillo78/fantasy_beyond/character/customization/race/merfolk/MerfolkEyes.java
@@ -4,12 +4,22 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import grillo78.fantasy_beyond.FantasyBeyond;
 import grillo78.fantasy_beyond.character.customization.PlayerCustomization;
 import grillo78.fantasy_beyond.character.customization.race.Coloreable;
+import grillo78.fantasy_beyond.client.clothes.RacistClothItemRenderer;
+import grillo78.fantasy_beyond.client.texture.AlphaMaskTexture;
+import grillo78.fantasy_beyond.items.RacistClothItem;
 import net.minecraft.client.model.PlayerModel;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
+import top.theillusivec4.curios.api.type.capability.ICuriosItemHandler;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MerfolkEyes extends MerfolkCharacteristic implements Coloreable {
 
@@ -41,20 +51,16 @@ public class MerfolkEyes extends MerfolkCharacteristic implements Coloreable {
     @Override
     public ResourceLocation getTexture(Player player) {
         ResourceLocation baseTexture = renderingIris ? ResourceLocation.fromNamespaceAndPath(FantasyBeyond.MOD_ID, "textures/entity/customization/race/common/iris/" + getVariant() + ".png") : ResourceLocation.fromNamespaceAndPath(FantasyBeyond.MOD_ID, "textures/entity/customization/race/common/eyes/" + getVariant() + ".png");
-//
-//        List<ResourceLocation> masks = new ArrayList<>();
-//        AtomicReference<String> append = new AtomicReference<>("");
-//        player.getCapability(ClothesProvider.CLOTHES_INVENTORY).ifPresent(clothes -> {
-//            for (int i = 0; i < clothes.getInventory().getSlots(); i++) {
-//                Item item = clothes.getInventory().getStackInSlot(i).getItem();
-//                if (item instanceof Cloth) {
-//                    masks.add(((Cloth) item).getAlphaMask(player));
-//                    append.set(append.get() + ForgeRegistries.ITEMS.getKey(item).toString().replace(":", "_"));
-//                }
-//            }
-//        });
-//
-//        return AlphaMaskTexture.getTexture(baseTexture, ResourceLocation.parse(ClothesMod.MOD_ID, baseTexture.getPath() + append), masks);
-        return baseTexture;
+
+        List<ResourceLocation> masks = new ArrayList<>();
+        AtomicReference<String> append = new AtomicReference<>("");
+
+        ICuriosItemHandler inventory = CuriosApi.getCuriosInventory(player).get();
+        for (SlotResult slot : inventory.findCurios(itemStack -> itemStack.getItem() instanceof RacistClothItem)) {
+            masks.add(RacistClothItemRenderer.getAlphaMask(player, (RacistClothItem) slot.stack().getItem()));
+            append.set(append.get() + BuiltInRegistries.ITEM.getKey(slot.stack().getItem()).toString().replace(":", "_"));
+        }
+
+        return AlphaMaskTexture.getTexture(baseTexture, ResourceLocation.fromNamespaceAndPath(FantasyBeyond.MOD_ID, baseTexture.getPath() + append), masks);
     }
 }
